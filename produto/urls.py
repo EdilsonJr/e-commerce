a5c0a5d39ac8e7bef77e6756a0ff6b293d0d478d
Edilson <edilsoncljr@gmail.com>
2022-05-21 17:43:51 -0300

    urls

---
+++ produto/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+
+app_name = 'produto'
+
+urlpatterns = [
+    path('', views.ListaProdutos.as_view(), name='lista'),
+    path('<slug>', views.DetalheProduto.as_view(), name='detalhe'),
+    path('addaocarinnho/', views.AddAoCarrinho.as_view(), name='addaocarinnho'),
+    path('removerdocarinnho/', views.RemoverDoCarrinho.as_view(), name='removerdocarinnho'),
+    path('carinnho/', views.Carrinho.as_view(), name='carinnho'),
+    path('finalizar/', views.Finalizar(), name='finalizar'),
+]
--- produto/views.py
+++ produto/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views import View
 
-# Create your views here.
+
+class ListaProdutos(ListView):
+    pass
+
+
+class DetalheProduto(View):
+    pass
+
+
+class AddAoCarrinho(View):
+    pass
+
+
+class RemoverDoCarrinho(View):
+    pass
+
+
+class Carrinho(View):
+    pass
+
+
+class Finalizar(View):
+    pass
