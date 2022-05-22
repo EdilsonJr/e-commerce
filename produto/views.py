b43062e8e521851695a9a7088d4f6ba15d58f2f8
Edilson <edilsoncljr@gmail.com>
2022-05-22 16:31:28 -0300

    detalhes do produto

--- produto/views.py
+++ produto/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from django.views import View
+from django.http import HttpResponse
 from . import models
+
 
 class ListaProdutos(ListView):
     model = models.Produto
@@ -10,8 +13,11 @@ class ListaProdutos(ListView):
     paginate_by = 10
 
 
-class DetalheProduto(View):
-    pass
+class DetalheProduto(DetailView):
+    model = models.Produto
+    template_name = 'produto/detalhe.html'
+    context_object_name = 'produto'
+    slug_url_kwarg = 'slug'
 
 
 class AddAoCarrinho(View):
