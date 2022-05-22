6b31d8b109b7df93cb1c309935e468dfda753c14
Edilson <edilsoncljr@gmail.com>
2022-05-22 15:06:41 -0300

    pagination

--- produto/views.py
+++ produto/views.py
@@ -7,6 +7,7 @@ class ListaProdutos(ListView):
     model = models.Produto
     template_name = 'produto/lista.html'
     context_object_name = 'produtos'
+    paginate_by = 10
 
 
 class DetalheProduto(View):
